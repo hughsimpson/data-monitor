e3019cea309eac122ffb4002875fdb4ecab2e85d
Hugh Simpson <hughrsimpson@gmail.com>
2013-10-21 15:15:27 +0100

    brief cam

--- src/main/scala/monitor/DataDogCalls.scala
+++ src/main/scala/monitor/DataDogCalls.scala
@@ -1,6 +1,7 @@
 package monitor
 
 import com.timgroup.statsd.{NonBlockingStatsDClient, StatsDClient}
+import akka.actor.ActorRef
 
 object DataDogCalls {
   private final val statsd: StatsDClient = new NonBlockingStatsDClient("", "localhost", 8125, Array[String]("tag:value"))
@@ -9,6 +10,7 @@ object DataDogCalls {
     case i:Int if 150 < i && i<200 => statsd.incrementCounter("message.between150And200")
     case i:Int => statsd.incrementCounter("message.otherNumber")
     case i:String => statsd.incrementCounter("message.UnknownString")
+    case (sender: ActorRef, i: Int) => statsd.recordHistogramValue("message.fromTuple",i)
     case x => statsd.incrementCounter(s"message.${x.getClass.toString}")
   }}
   def logFailure() {statsd.incrementCounter("message.failures")}
