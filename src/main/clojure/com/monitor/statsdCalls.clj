ec562472a36d3eac70ad01df15c2a73cec7b0062
Hugh Simpson <hughrsimpson@gmail.com>
2013-10-28 10:43:02 +0000

    looks like it might work

--- src/main/clojure/com/monitor/statsdCalls.clj
+++ src/main/clojure/com/monitor/statsdCalls.clj
@@ -25,7 +25,7 @@
 (def static-udp-server []
   (DatagramSocket. port))
 
-; use these if it makes more sense to redeclare the udp server that simply hold it static....
+; use these if it makes more sense to redeclare the udp server than simply hold it static....
 (defn create-udp-server []
   (DatagramSocket. port))
 (defn start-udp-server []
@@ -48,10 +48,11 @@
 ; == send(String.format("%s%s:%d|c%s", prefix, s, delta, tagString(args))) --so, need to create this formatted string
 ; FIRST: tagString = "" if no args and no 'constant tags'
 ; Otherwise tagString = "|#${tag1},${tag2},...." where 'constant tags' come before any args.Also, constant tags is probably gonna be null, tbh.
-(defn tagString [& args]
-  (if (= (length args) 0)       ; in progress!!! not sure on if _or_ fold syntax
-    ""
-    (reduce-kv (a b -> a + "|" + b) "|#" args))
+(defn tagString [args]
+  (if (= (alength args) 0)       ; in progress!!! not sure on if _or_ fold syntax
+    ""                   ; brackets or no brackets?
+    (str "|#" (reduce str args))
+  )
   )
 ;
 ; probably like... s"${prefix}${s}:1|c${tagString}" = (format "%s%s:%d|c%s" prefix s 1 tagString) -- again, prefix is currently ""
